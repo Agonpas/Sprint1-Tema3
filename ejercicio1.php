3481e1373aef3a70f7cf4b5d6a3bb9418b6037e8
Agonpas <agonpas@gmail.com>
2023-11-08 10:48:23 +0100

    Ejercicio 1 array

---
+++ ejercicio1.php
@@ -0,0 +1,13 @@
+<?php
+$numeros = array(); /*creamos array */
+$numeros = [1, 3, 2, 5, 8]; /*llenamos el array */
+/*ahora mostrmos los 5 valores de uno en uno */
+echo $numeros [0] . "</p>";
+echo $numeros [1] . "</p>";
+echo $numeros [2] . "</p>";
+echo $numeros [3] . "</p>";
+echo $numeros [4] . "</p>";
+echo "<pre>";
+var_dump ($numeros);
+echo "<pre>";
+?>
