06d8e9c54498dacf9f89c12f3e98c65a88cff283
Agonpas <agonpas@gmail.com>
2023-11-08 20:36:36 +0100

    Nivel 1 acabado

---
+++ ejercicio4.php
@@ -0,0 +1,8 @@
+<?php
+/*creamos el array asociativo con los datos solicitados*/
+$informacion = array('nombre' => 'Alberto', 'edad' => 49, 'email' => 'agonpas@gmail.com', 'comida favorita' => 'canelones');
+/* visualizamos todos los datos del array*/
+echo "<pre>";
+var_dump ($informacion);
+echo "<pre>";
+?>
