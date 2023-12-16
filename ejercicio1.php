1cdd75d193d9f921cac00922677313b6f0eb1759
Agonpas <agonpas@gmail.com>
2023-12-16 13:15:15 +0100

    Ejercicio 1 con un sistema mejor

--- ejercicio1.php
+++ ejercicio1.php
@@ -8,7 +8,18 @@ echo $numeros [2] . "\n";
 echo $numeros [3] . "\n";
 echo $numeros [4] . "\n";
 /*mostramos toda la info del array: numero de variables, posición, tipo de variable y los valores de estas*/
-echo "<pre>";
+echo PHP_EOL;
 var_dump ($numeros);
-echo "<pre>";
+echo PHP_EOL;
+
+sistemaMejor($numeros);
+
+function sistemaMejor($numeros){
+    $i = 0;
+    foreach ($numeros as $i=> $numero) {
+        echo "La cantidad contenida en la posición " . $i . " es de " . $numero . PHP_EOL;
+        $i++;
+        echo PHP_EOL;
+    }
+}
 ?>
